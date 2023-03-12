679f477c3ee928ca0c6412570c6294d4607f8146
Raphael Neumann <raphael@rneumann.me>
2023-03-12 01:04:31 -0300

    feature: make possible to generate release notes on dry-run execution

--- release.config.js
+++ release.config.js
@@ -2,23 +2,30 @@ const plugins = require("./plugins");
 const { execSync } = require('child_process');
 
 
- function getLocalRepoUrl() {
-    const topLevelDir = execSync('git rev-parse --show-toplevel')
-      .toString()
-      .trim();
-  
-    return `file://${topLevelDir}/.git`;
-  }
-  
-  function getCurrentBranch() {
-    return execSync('git rev-parse --abbrev-ref HEAD')
-      .toString()
-      .trim();
-  }
+function getLocalRepoUrl() {
+  const topLevelDir = execSync('git rev-parse --show-toplevel').toString().trim();
+  return `file://${topLevelDir}/.git`;
+}
 
-module.exports = {
-	repositoryUrl: getLocalRepoUrl(),
-  branches: getCurrentBranch(),
-	releaseRules: plugins[0][1].releaseRules,
+function getCurrentBranch() {
+  return execSync('git rev-parse --abbrev-ref HEAD')
+    .toString()
+    .trim();
+}
+
+releaseConfig = {
 	plugins
 };
+if ( process.argv.includes('--dry-run') ) {
+  releaseConfig.branches = getCurrentBranch();
+  releaseConfig.repositoryUrl = getLocalRepoUrl();
+}
+
+module.exports = releaseConfig
+
+//module.exports = {
+//	
+//  branches: getCurrentBranch(),
+//	releaseRules: plugins[0][1].releaseRules,
+//	plugins
+//};
